36ea1306995113657da5ba00f806d41d8ac94d91
Egor Gututui <102786178+TamagoNier@users.noreply.github.com>
2023-10-10 19:32:58 +0200

    ajout du cache attention à exclure de commit

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+use App\Classes\Metier\Employe;
+require '..\vendor\autoload.php';
+
+$e = new Employe();
---
+++ src/classes/metier/Employe.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Classes\Metier;
+
+Class Employe {
+    public function __construct() {
+        echo "Autoload OK";
+    }
+}
