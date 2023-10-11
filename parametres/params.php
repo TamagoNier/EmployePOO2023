a11bddcd77086ff26d54174ac1cc02dd93c5fb42
Egor GUTUTUI <egor.gututui@lyc-bonaparte.fr>
2023-10-11 15:51:44 +0200

    creation app exception et params

---
+++ parametres/params.php
@@ -0,0 +1,3 @@
+<?php
+define('Egor', APP_USER);
+define('Employe RH',APP_NAME );
--- src/classes/metier/Employe.php
+++ src/classes/metier/Employe.php
@@ -2,7 +2,5 @@
 namespace App\Classes\Metier;
 
 Class Employe {
-    public function __construct() {
-        echo "Autoload OK";
-    }
+    
 }
